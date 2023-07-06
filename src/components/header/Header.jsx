fe453429d43497a39182ee83b836cc5a200a171f
jayden-n <jaydennguyen.dev@gmail.com>
2023-07-05 23:14:57 -0400

    created + style: navigate & searching modal logic

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -17,20 +17,99 @@ const Header = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
+  const openSearch = () => {
+    setMobileMenu(false);
+    setShowSearch(true);
+  };
+
+  const openMobileMenu = () => {
+    setMobileMenu(true);
+    setShowSearch(false);
+  };
+
+  // Navigate to either 'movie' or 'tv'
+  const navigationHandler = (type) => {
+    if (type === 'movie') {
+      navigate('/explore/movie');
+    } else {
+      navigate('/explore/tv');
+    }
+    // then closes after being navigated
+    setMobileMenu(false);
+  };
+
+  // searching thru heading
+  const searchQueryHandler = (event) => {
+    if ((event.key === 'Enter' || event.keyCode === 13) && query.length > 0) {
+      navigate(`/search/${query}`);
+      setTimeout(() => {
+        setShowSearch(false);
+      }, 1000);
+    }
+  };
+
   return (
-    <header className="header">
+    <header className={`header ${mobileMenu ? 'mobileView' : ''} ${show}`}>
       <ContentWrapper>
         <div className="logo">
-          <p>StreamFlix</p>
+          <a href="/" style={{ textDecoration: 'none' }}>
+            <p>StreamFlix</p>
+          </a>
         </div>
         <ul className="menuItems">
-          <li className="menuItem">Movies</li>
-          <li className="menuItem">TV Shows</li>
+          <li
+            className="menuItem"
+            onClick={() => {
+              navigationHandler('movie');
+            }}
+          >
+            Movies
+          </li>
+          <li
+            className="menuItem"
+            onClick={() => {
+              navigationHandler('tv');
+            }}
+          >
+            TV Shows
+          </li>
           <li className="menuItem">
-            <HiOutlineSearch />
+            <HiOutlineSearch onClick={openSearch} />
           </li>
         </ul>
+        <div className="mobileMenuItems">
+          <HiOutlineSearch onClick={openSearch} />
+          {mobileMenu ? (
+            <VscChromeClose
+              onClick={() => {
+                setMobileMenu(false);
+              }}
+            />
+          ) : (
+            <SlMenu onClick={openMobileMenu} />
+          )}
+        </div>
       </ContentWrapper>
+      {/*Conditional search input*/}
+      {showSearch && (
+        <div className="searchBar">
+          <ContentWrapper>
+            <div className="searchInput">
+              <input
+                type="text"
+                placeholder="Search for a movie or TV show..."
+                onChange={(e) => setQuery(e.target.value)}
+                onKeyUp={searchQueryHandler}
+              />
+              <VscChromeClose
+                onClick={() => {
+                  setShowSearch(false);
+                }}
+              />
+            </div>
+          </ContentWrapper>
+        </div>
+      )}
     </header>
   );
 };
