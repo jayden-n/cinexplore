e09eebda71f887c768dc08788d258c9e02b7a34c
jayden-n <jaydennguyen.dev@gmail.com>
2023-07-06 18:08:40 -0400

    added: switching between tabs function

--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -1,10 +1,23 @@
+import { useState } from 'react';
 import ContentWrapper from '../../../components/contentWrapper/ContentWrapper';
+import SwitchTabs from '../../../components/switchTabs/SwitchTabs';
+import useFetch from '../../../hooks/useFetch';
 
 const Trending = () => {
+  const [endPoint, setEndPoint] = useState('day');
+
+  const { data, loading } = useFetch(`/trending/all/${endPoint}`);
+
+  const onTabChange = (tab) => {
+    setEndPoint(tab === 'Daily' ? 'day' : 'week');
+  };
+
   return (
     <div className="carouselSection">
       <ContentWrapper>
         <span className="carouselTitle">Trending</span>
+
+        <SwitchTabs data={['Daily', 'Weekly']} onTabChange={onTabChange} />
       </ContentWrapper>
     </div>
   );
