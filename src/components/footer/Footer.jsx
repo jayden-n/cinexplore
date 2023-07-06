08c0db228403083332b2735c93df0b771e5c0904
jayden-n <jaydennguyen.dev@gmail.com>
2023-07-06 02:09:03 -0400

    created: footer file structure component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,49 @@
-import React from 'react';
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaTwitter,
+  FaLinkedin,
+} from 'react-icons/fa';
+
+import ContentWrapper from '../contentWrapper/ContentWrapper';
+
 import './Footer.styles.scss';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <ContentWrapper>
+        <ul className="menuItems">
+          <li className="menuItem">Terms Of Use</li>
+          <li className="menuItem">Privacy-Policy</li>
+          <li className="menuItem">About</li>
+          <li className="menuItem">Blog</li>
+          <li className="menuItem">FAQ</li>
+        </ul>
+        <div className="infoText">
+          We believe that movies have a special ability to transport us to
+          different worlds, stir our emotions, and ignite our imagination. They
+          have the power to entertain, inspire, and create connections between
+          people from all walks of life. So grab some popcorn, settle in, and
+          embark on an incredible cinematic journey with us!
+        </div>
+        <div className="socialIcons">
+          <span className="icon">
+            <FaFacebookF />
+          </span>
+          <span className="icon">
+            <FaInstagram />
+          </span>
+          <span className="icon">
+            <FaTwitter />
+          </span>
+          <span className="icon">
+            <FaLinkedin />
+          </span>
+        </div>
+      </ContentWrapper>
+    </footer>
+  );
 };
 
 export default Footer;
