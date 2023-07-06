f1d21a037808617e177c3f645bba803fb4e9c008
jayden-n <jaydennguyen.dev@gmail.com>
2023-07-05 22:39:43 -0400

    created: heading structures component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,38 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { HiOutlineSearch } from 'react-icons/hi';
+import { SlMenu } from 'react-icons/sl';
+import { VscChromeClose } from 'react-icons/vsc';
+import { useNavigate, useLocation } from 'react-router-dom';
+
 import './Header.styles.scss';
 
+import ContentWrapper from '../contentWrapper/ContentWrapper';
+
 const Header = () => {
-  return <div>Header</div>;
+  const [show, setShow] = useState('top');
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [query, setQuery] = useState('');
+  const [showSearch, setShowSearch] = useState('');
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  return (
+    <header className="header">
+      <ContentWrapper>
+        <div className="logo">
+          <p>StreamFlix</p>
+        </div>
+        <ul className="menuItems">
+          <li className="menuItem">Movies</li>
+          <li className="menuItem">TV Shows</li>
+          <li className="menuItem">
+            <HiOutlineSearch />
+          </li>
+        </ul>
+      </ContentWrapper>
+    </header>
+  );
 };
 
 export default Header;
