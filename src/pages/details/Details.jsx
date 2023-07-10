857a12a5d5ad92c4b657d1fa0b826f399be94a77
jayden-n <jaydennguyen.dev@gmail.com>
2023-07-10 19:07:12 -0400

    added: movie recommendations carousel section

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -9,6 +9,7 @@ import DetailsBanner from './detailsBanner/DetailsBanner';
 import Cast from './cast/Cast';
 import VideosSection from './videosSection/VideosSection';
 import Similar from './carousels/Similar';
+import Recommendation from './carousels/Recommendation';
 
 const Details = () => {
   // retrieves route parameter (useParams)
@@ -26,6 +27,7 @@ const Details = () => {
       <Cast data={credits?.cast} loading={creditsLoading} />
       <VideosSection data={data} loading={loading} />
       <Similar mediaType={mediaType} id={id} />
+      <Recommendation mediaType={mediaType} id={id} />
     </div>
   );
 };
