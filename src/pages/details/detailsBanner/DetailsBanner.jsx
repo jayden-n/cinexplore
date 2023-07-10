450a051d6ef331fbd25fa05bd22c5047dc6a2d59
jayden-n <jaydennguyen.dev@gmail.com>
2023-07-10 18:02:43 -0400

    implemented: trailer popup leveraging ReactPlayer

--- src/pages/details/detailsBanner/DetailsBanner.jsx
+++ src/pages/details/detailsBanner/DetailsBanner.jsx
@@ -12,8 +12,12 @@ import CircleRating from '../../../components/circleRating/CircleRating';
 import Img from '../../../components/lazyLoadImage/Img.jsx';
 import PosterFallback from '../../../assets/no-poster.png';
 import { PlayBtn } from '../PlayBtn';
+import VideoPopup from '../../../components/videoPopup/VideoPopup';
 
 const DetailsBanner = ({ video, crew }) => {
+  const [show, setShow] = useState(false);
+  const [videoId, setVideoId] = useState(null);
+
   // retrieves route parameter (useParams)
   const { mediaType, id } = useParams();
 
@@ -75,7 +79,13 @@ const DetailsBanner = ({ video, crew }) => {
                       <CircleRating rating={data.vote_average.toFixed(1)} />
 
                       {/* Movie Trailer */}
-                      <div className="playbtn" onClick={() => {}}>
+                      <div
+                        className="playbtn"
+                        onClick={() => {
+                          setShow(true);
+                          setVideoId(video.key);
+                        }}
+                      >
                         <PlayBtn />
                         <span className="text">Watch Trailer!</span>
                       </div>
@@ -157,6 +167,12 @@ const DetailsBanner = ({ video, crew }) => {
                     )}
                   </div>
                 </div>
+                <VideoPopup
+                  show={show}
+                  setShow={setShow}
+                  videoId={videoId}
+                  setVideoId={setVideoId}
+                />
               </ContentWrapper>
             </Fragment>
           )}
