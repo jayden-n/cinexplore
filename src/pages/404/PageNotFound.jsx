58282444ea1252868ca05339630bbb8db34aadd7
jayden-n <jaydennguyen.dev@gmail.com>
2023-07-10 23:36:53 -0400

    added: 404 page error with different route

--- src/pages/404/PageNotFound.jsx
+++ src/pages/404/PageNotFound.jsx
@@ -1,8 +1,16 @@
-import React from 'react';
 import './PageNotFound.styles.scss';
 
+import ContentWrapper from '../../components/contentWrapper/ContentWrapper';
+
 const PageNotFound = () => {
-  return <div>PageNotFound</div>;
+  return (
+    <div className="pageNotFound">
+      <ContentWrapper>
+        <span className="bigText">404 Error</span>
+        <span className="smallText">Page Not Found!</span>
+      </ContentWrapper>
+    </div>
+  );
 };
 
 export default PageNotFound;
