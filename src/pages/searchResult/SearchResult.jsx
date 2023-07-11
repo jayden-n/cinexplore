b017699ab18d8e1f8f5e3996a284b6d9e8c6203a
jayden-n <jaydennguyen.dev@gmail.com>
2023-07-10 21:28:21 -0400

    created: handle search result & error

--- src/pages/searchResult/SearchResult.jsx
+++ src/pages/searchResult/SearchResult.jsx
@@ -1,8 +1,72 @@
-import React from 'react';
 import './SearchResult.styles.scss';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import InfiniteScroll from 'react-infinite-scroll-component';
+
+import { fetchDataFromApi } from '../../utils/api';
+import ContentWrapper from '../../components/contentWrapper/ContentWrapper';
+import Spinner from '../../components/spinner/Spinner';
+import noResults from '../../assets/no-results.png';
 
 const SearchResult = () => {
-  return <div>SearchResult</div>;
+  const [data, setData] = useState(null);
+  const [pageNum, setPageNum] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const { query } = useParams();
+
+  const fetchInitialData = () => {
+    setLoading(true);
+    fetchDataFromApi(`/search/multi?query=${query}&page=${pageNum}`).then(
+      (res) => {
+        setData(res);
+        setPageNum((prev) => prev + 1);
+        setLoading(false);
+      }
+    );
+  };
+
+  const fetchNextPageData = () => {
+    fetchDataFromApi(`/search/multi?query=${query}&page=${pageNum}`).then(
+      (res) => {
+        if (data?.result) {
+          setData({
+            ...data,
+            result: [...data?.results, ...res.results],
+          });
+        } else {
+          setData(res);
+        }
+        setPageNum((prev) => prev + 1);
+      }
+    );
+  };
+
+  useEffect(() => {
+    fetchInitialData();
+  }, [query]);
+
+  return (
+    <div className="searchResultsPage">
+      {loading && <Spinner initial={true} />}
+      {!loading && (
+        <ContentWrapper>
+          {data?.results.length > 0 ? (
+            <>
+              <div className="pageTitle">
+                {`Search ${
+                  data.total_results > 1 ? 'results' : 'result'
+                } of '${query}'`}
+              </div>
+            </>
+          ) : (
+            <span className="resultNotFound">
+              Sorry, there is no result for &apos;${query}&apos;
+            </span>
+          )}
+        </ContentWrapper>
+      )}
+    </div>
+  );
 };
 
 export default SearchResult;
